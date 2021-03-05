058e0a7631cabd83561ad3479cc693f5e60fcc90
Theodor Har <theodorlehar@gmail.com>
2021-03-05 10:14:10 -0800

    Deleted files now within 1.0 branch from Master

--- Podfile
+++
@@ -1,5 +0,0 @@
-target 'MealME' do
-	use_frameworks!
-	pod 'AlamofireImage'
-end
-
--- Pods/Target Support Files/Pods-MealME/Pods-MealME-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MealME : NSObject
-@end
-@implementation PodsDummy_Pods_MealME
-@end
