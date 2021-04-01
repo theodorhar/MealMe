311084278f13d9531d58aa1fab09a28d09c4e829
Theodor Har <theodorlehar@gmail.com>
2021-04-01 16:33:33 -0700

    "updated add_cross to handle >2 columns"

--- cofi_model.py
+++ cofi_model.py
@@ -15,17 +15,16 @@ recipe_lookup = data_processing.get_recipe_lookup(raw_data, debug=DEBUG)
 feature_columns = []
 #feature column definition
 #adds a cross of two (bucketed) columns from a dataframe to the feature_columns list
-def add_cross(data:pd.DataFrame, feature_columns:list, col_one:str, col_two:str, num_buckets = 10, num_buckets_two = 10, hash_bucket_size = 100) -> None:
-
-    numeric_col_one = tf.feature_column.numeric_column(col_one)
-    buckets_one = pd.qcut(data[col_one], q=num_buckets, retbins=True) 
-    feature_col_one = tf.feature_column.bucketized_column(numeric_col_one,list(buckets_one[1]))
-
-    numeric_col_two = tf.feature_column.numeric_column(col_two)
-    buckets_two = pd.qcut(data[col_two], q=num_buckets_two, retbins=True) 
-    feature_col_two = tf.feature_column.bucketized_column(numeric_col_two,list(buckets_two[1]))
-
-    cross = tf.feature_column.crossed_column([feature_col_one,feature_col_two],hash_bucket_size=hash_bucket_size) 
+def add_cross(data:pd.DataFrame, feature_columns:list, cols:list, num_buckets:list, hash_bucket_size = 100) -> None:
+    if len(cols) != len(num_buckets):
+        raise ValueError("len of cols(" + str(len(cols)) + ") must equal len of num_buckets("+ str(len(num_buckets))+").")
+    fcols = []
+    for col,bnum in zip(cols,num_buckets):
+        numeric_col = tf.feature_column.numeric_column(col)
+        buckets_one = pd.qcut(data[col], q=bnum, retbins=True) 
+        feature_col = tf.feature_column.bucketized_column(numeric_col,list(buckets_one[1]))
+        fcols.append(feature_col)
+    cross = tf.feature_column.crossed_column(fcols,hash_bucket_size=hash_bucket_size) 
     crossed_features = tf.feature_column.indicator_column(cross)
 
     feature_columns.append(crossed_features)
@@ -37,7 +36,7 @@ def add_bucketed_col(data:pd.DataFrame, feature_columns:list, col:str,num_bucket
     feature_columns.append(feature_col)
 
 add_bucketed_col(recipes,feature_columns,"rating",10)
-add_cross(recipes, feature_columns, "directions_bul_len", "directions_char_len", 10, 10, 100)
+add_cross(recipes, feature_columns, ["directions_sent_len", "directions_char_len"], [10,10], 100)
 #add_cross(recipes,feature_columns,"prep","cook",7,7,100)
 
 
