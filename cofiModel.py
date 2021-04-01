ea73a9773bc5c7dee6ed7091396496c819cc7929
Theodor Har <theodorlehar@gmail.com>
2021-03-31 22:46:43 -0700

    renamed and reordered files

--- cofiModel.py
+++ cofiModel.py
@@ -1,4 +1,7 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import processData
 
+raw_data = processData.get_raw_data()
+print(raw_datac)
---
+++ get_raw_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import json
+from glob import glob
+
+#generates a dataframe of raw data
+def get_raw_data() -> pd.DataFrame:
+    columns = ['url', 'name', 'rating', 'ingredients', 'directions', 'prep', 'cook', 'ready in', 'calories', 'ratingcount']
+    raw_data = pd.DataFrame(data=[], columns=columns)
+    frames = []
+    for file_name in glob('*.json'):
+        with open(file_name) as f:
+            df = pd.read_json(f)
+            frames.append(df)
+    raw_data = pd.concat(frames,axis = 0, sort = True)
+    raw_data.drop_duplicates(inplace = True)
+    return raw_data
