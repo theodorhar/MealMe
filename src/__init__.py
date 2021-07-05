d42456d83a86599ef7b1da5fa780ebe1b305fc5d
Theodor Har <theodorlehar@gmail.com>
2021-07-04 21:48:44 -0400

    add default api query

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 
-import os
+import os, random
 from flask import Flask,jsonify
 from flask_restx import Api,Resource
 from src.load_data import get_recipe_lookup
@@ -15,7 +15,9 @@ app.config.from_object(app_settings)
 
 #load entire dataset into memory
 lookup = get_recipe_lookup(debug = DEBUG)
-
+#construct favored
+querystr = 'id < 62343 and review_count > 3000 or id > 80859 and review_count > 500'
+favored = lookup.query(querystr)
 # define services
 class Ping(Resource):
     def get(self):
@@ -30,12 +32,28 @@ class GetCard(Resource):
         card = dict()
         recipe = lookup.query('id ==' + str(recipe_id)).to_dict()
         #title,url,photo_url,rating_stars,review_count,cook_time_minutes,id
-        card['title'] = recipe['title']
-        card['url'] = recipe['url']
-        card['photo_url'] = recipe['photo_url']
-        card['rating_stars'] = recipe['rating_stars']
-        card['review_count'] = recipe['review_count']
-        card['cook_time_minutes'] = recipe['cook_time_minutes']
-        card['id'] = recipe['id']
+        card['title'] = recipe['title'][recipe_id]
+        card['url'] = recipe['url'][recipe_id]
+        card['photo_url'] = recipe['photo_url'][recipe_id]
+        card['rating_stars'] = recipe['rating_stars'][recipe_id]
+        card['review_count'] = recipe['review_count'][recipe_id]
+        card['cook_time_minutes'] = recipe['cook_time_minutes'][recipe_id]
+        card['id'] = recipe['id'][recipe_id]
         return jsonify(card)
 api.add_resource(GetCard, '/recipecard/<int:recipe_id>/')
+
+class default(Resource):
+    def get(self):
+        if len(favored) > 10:
+            indices = []
+            alreadyDisplayed = []
+            for _ in range(10):
+                index = random.randint(0,len(favored)-1)
+                while(index in alreadyDisplayed):
+                    index = random.randint(0,len(favored)-1)
+                indices.append(index)
+                
+            return jsonify(favored.iloc[indices].to_dict())
+        else:
+            return jsonify(favored.to_dict())
+api.add_resource(default, '/default')
