8bd06b373ce323ed4c5924fbe5fbc9d41c90ac66
Theodor Har <theodorlehar@gmail.com>
2021-08-19 08:53:03 -0700

    remove all authentication related code

--- services/web/api/__init__.py
+++ services/web/api/__init__.py
@@ -1,16 +1,8 @@
 
-import os, random, requests, json
-from flask import Flask,jsonify,Response,request,url_for,redirect
-from flask_login import (
-    LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
-)
+import os, random
+from flask import Flask,jsonify,Response,request
 from flask_restx import Api,Resource
 from mysql.connector import pooling
-from oauthlib.oauth2 import WebApplicationClient
 
 from pandas.core.frame import DataFrame
 from rapidfuzz import fuzz
@@ -20,17 +12,8 @@ from rapidfuzz import process
 from db.user import User
 from api.load_data import get_recipe_lookup
 
-# Configuration
+# configuration
 DEBUG = False
-google_client_id_file = open('/run/secrets/google-client-id', 'r')
-GOOGLE_CLIENT_ID = google_client_id_file.read()
-google_client_id_file.close()
-google_client_secret_file = open('/run/secrets/google-client-secret', 'r')
-GOOGLE_CLIENT_SECRET = google_client_secret_file.read()
-google_client_secret_file.close()
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
 
 # instantiate the app
 app = Flask(__name__)
@@ -42,6 +25,7 @@ app.config.from_object(app_settings)
 app.secret_key = os.urandom(24)
 #load entire dataset into memory
 lookup = get_recipe_lookup(debug = DEBUG)
+
 #MySQL connection pool initialization
 pf = open('/run/secrets/db-users-password', 'r')
 userdbconfig = {
@@ -53,21 +37,6 @@ userdbconfig = {
 userpool = pooling.MySQLConnectionPool(pool_name = "userpool", pool_size = 20, **userdbconfig)
 pf.close()
 
-#login session handler
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-# Flask-Login helper to retrieve a user from our db
-@login_manager.user_loader
-def load_user(user_id):
-    conn = userpool.get_connection()
-    user = User.get(conn,user_id)
-    conn.close()
-    return user
-
-# OAuth 2 client setup
-client = WebApplicationClient(GOOGLE_CLIENT_ID)
-
 #data preprocessing
 #construct favored
 querystr = 'id < 71906 and review_count > 3000 or id > 90422 and review_count > 400'
@@ -75,112 +44,15 @@ favored = lookup.query(querystr)
 
 # define API services
 
+class Login(Resource):
+    def get(self,user_token):
+        pass
 class Ping(Resource):
     def get(self):
         return {
         'message': 'pong!'
     }
 api.add_resource(Ping, '/ping')
-
-class Home(Resource):
-    def get(self):
-        if current_user.is_authenticated:
-            return (
-                "<p>Hello, {}! You're logged in! Email: {}</p>"
-                "<div><p>Google Profile Picture:</p>"
-                '<img src="{}" alt="Google profile pic"></img></div>'
-                '<a class="button" href="/logout">Logout</a>'.format(
-                    current_user.name, current_user.email, current_user.profile_pic
-                )
-            )
-        else:
-            return '<a class="button" href="/login">Google Login</a>'
-api.add_resource(Home, "/index")
-
-def get_google_provider_cfg():
-    return requests.get(GOOGLE_DISCOVERY_URL).json()
-
-class Login(Resource):
-    def get(self):
-        # Find out what URL to hit for Google login
-        google_provider_cfg = get_google_provider_cfg()
-        authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-
-        # Use library to construct the request for Google login and provide
-        # scopes that let you retrieve user's profile from Google
-        request_uri = client.prepare_request_uri(
-            authorization_endpoint,
-            redirect_uri= request.base_url + "/callback",
-            scope=["openid", "email", "profile"],
-        )
-        return redirect(request_uri)
-api.add_resource(Login ,"/login")
-
-class Login_Callback(Resource):
-    def get(self):
-        # Get authorization code Google sent back to you
-        code = request.args.get("code")
-        # Find out what URL to hit to get tokens that allow you to ask for
-        # things on behalf of a user
-        google_provider_cfg = get_google_provider_cfg()
-        token_endpoint = google_provider_cfg["token_endpoint"]
-        # Prepare and send a request to get tokens! Yay tokens!
-        token_url, headers, body = client.prepare_token_request(
-            token_endpoint,
-            authorization_response=request.url,
-            redirect_url=request.base_url,
-            code=code
-        )
-        token_response = requests.post(
-            token_url,
-            headers=headers,
-            data=body,
-            auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
-        )
-
-        # Parse the tokens!
-        client.parse_request_body_response(json.dumps(token_response.json()))
-        # Now that you have tokens (yay) let's find and hit the URL
-        # from Google that gives you the user's profile information,
-        # including their Google profile image and email
-        userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-        uri, headers, body = client.add_token(userinfo_endpoint)
-        userinfo_response = requests.get(uri, headers=headers, data=body)
-        # You want to make sure their email is verified.
-        # The user authenticated with Google, authorized your
-        # app, and now you've verified their email through Google!
-        if userinfo_response.json().get("email_verified"):
-            unique_id = userinfo_response.json()["sub"]
-            users_email = userinfo_response.json()["email"]
-            picture = userinfo_response.json()["picture"]
-            users_name = userinfo_response.json()["given_name"]
-        else:
-            return "User email not available or not verified by Google.", 400
-        # Create a user in your db with the information provided
-        # by Google
-        user = User(
-            id_=unique_id, name=users_name, email=users_email, profile_pic=picture
-        )
-
-        # Doesn't exist? Add it to the database.
-        conn = userpool.get_connection()
-        if not User.get(conn,unique_id):
-            User.create(conn,unique_id, users_name, users_email, picture)
-        conn.close()
-        # Begin user session by logging the user in
-        login_user(user)
-
-        # Send user back to homepage
-        return redirect(url_for("home"))
-
-api.add_resource(Login_Callback, "/login/callback")
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("home"))
-
 
 class Card(Resource):
     def get(self,recipe_id):
--- services/web/db/user.py
+++ services/web/db/user.py
@@ -1,125 +1,126 @@
-from flask_login import UserMixin
-from mysql.connector.connection import MySQLConnection
 
 #needs access to: current recipes database to fetch recipes
-class User(UserMixin):
-    def __init__(self, id_, name, email, profile_pic):
-        self.id = id_
-        self.name = name
-        self.email = email
-        self.profile_pic = profile_pic
+from pandas import DataFrame
+import numpy as np
+import collections
+class User():
+    # def __init__(self, id_, name, email, profile_pic):
+    #     self.id = id_
+    #     self.name = name
+    #     self.email = email
+    #     self.profile_pic = profile_pic
 
-    @staticmethod
-    def get(connection: MySQLConnection, user_id: int):
-        if connection == None:
-            raise ConnectionError("Database connection is invalid")
-        db = connection.cursor()
-        db.execute(
-            "SELECT * FROM user WHERE id = %s", (user_id,)
-        )
-        user = db.fetchone()
-        if not user:
-            return None
+    # @staticmethod
+    # def get(connection: MySQLConnection, user_id: int):
+    #     if connection == None:
+    #         raise ConnectionError("Database connection is invalid")
+    #     db = connection.cursor()
+    #     db.execute(
+    #         "SELECT * FROM user WHERE id = %s", (user_id,)
+    #     )
+    #     user = db.fetchone()
+    #     if not user:
+    #         return None
 
-        user = User(
-            id_=user[0], name=user[1], email=user[2], profile_pic=user[3]
-        )
-        return user
+    #     user = User(
+    #         id_=user[0], name=user[1], email=user[2], profile_pic=user[3]
+    #     )
+    #     return user
 
-    @staticmethod
-    def create(connection: MySQLConnection, id_: int, name: str, email: str, profile_pic: str):
-        if connection == None:
-            raise ConnectionError("Database connection is invalid")
-        db = connection.cursor()
-        db.execute(
-            "INSERT INTO user (id, name, email, profile_pic) "
-            "VALUES (%s, %s, %s, %s)",
-            (id_, name, email, profile_pic),
-        )
-        connection.commit()
-    # def __init__(self):
-    #     self._age = None
-    #     self._name = None
-    #     self._location = None
-    #     self._id = None
-    #     self._recipes_viewed = []
-    #     self._recipes_made = []  #if it is in made it must be in viewed
-    #     self._recipes_liked = [] #if it is in liked it must be in viewed
-    #     self._ingredients_owned = collections.defaultdict(lambda : 0)
-    #     self._weight_matrix = np.array([2,2,1,5]) #like,made,view,own weights in order
-    # def set_age(self,age:int) -> None:
-    #     if type(age) != int:
-    #         raise TypeError("age type must be int- type: ",type(age))
-    #     if age < 0:
-    #         raise ValueError("age must be above 0- age:" ,age)
-    #     self._age = age
-    # def set_name(self,name:str) -> None:
-    #     self._name = name
-    # def set_id(self,id:int) -> None:
-    #     if type(id) != int:
-    #         raise TypeError("id type must be int- type: ",type(id))
-    #     self._int = id
+    # @staticmethod
+    # def create(connection: MySQLConnection, id_: int, name: str, email: str, profile_pic: str):
+    #     if connection == None:
+    #         raise ConnectionError("Database connection is invalid")
+    #     db = connection.cursor()
+    #     db.execute(
+    #         "INSERT INTO user (id, name, email, profile_pic) "
+    #         "VALUES (%s, %s, %s, %s)",
+    #         (id_, name, email, profile_pic),
+    #     )
+        # connection.commit()
+    def __init__(self):
+        self._age = None
+        self._name = None
+        self._location = None
+        self._id = None
+        self._recipes_viewed = []
+        self._recipes_made = []  #if it is in made it must be in viewed
+        self._recipes_liked = [] #if it is in liked it must be in viewed
+        self._ingredients_owned = collections.defaultdict(lambda : 0)
+        self._weight_matrix = np.array([2,2,1,5]) #like,made,view,own weights in order
+    def set_age(self,age:int) -> None:
+        if type(age) != int:
+            raise TypeError("age type must be int- type: ",type(age))
+        if age < 0:
+            raise ValueError("age must be above 0- age:" ,age)
+        self._age = age
+    def set_name(self,name:str) -> None:
+        self._name = name
+    def set_id(self,id:int) -> None:
+        if type(id) != int:
+            raise TypeError("id type must be int- type: ",type(id))
+        self._int = id
     
-    # def add_like(self,id:int) -> None:
-    #     if type(id) != int:
-    #         raise TypeError("id type must be int- type: ",type(id))
-    #     if id < 0:
-    #         raise ValueError("id must be above 0- id:" ,id)
-    #     self._recipes_liked.append(id)
-    # def add_view(self,id:int) -> None:
-    #     if type(id) != int:
-    #         raise TypeError("id type must be int- type: ",type(id))
-    #     if id < 0:
-    #         raise ValueError("id must be above 0- id:" ,id)
-    #     self._recipes_viewed.append(id)
-    # def add_make(self,id:int) -> None:
-    #     if type(id) != int:
-    #         raise TypeError("id type must be int- type: ",type(id))
-    #     if id < 0:
-    #         raise ValueError("id must be above 0- id:" ,id)
-    #     self._recipes_made.append(id)
-    # def update_ingredients(self,food:dict) -> None:
-    #     for x,count in food.items():
-    #         self._ingredients_owned[x] += count
-    # def update_weight_matrix(self,weights:list) -> None:
-    #     if type(weights) != list:
-    #         raise TypeError("type of weights must be list",type(weights))
-    #     if len(weights) != 4:
-    #         raise ValueError("list must be weight 4- len: ",len(weights))
-    #     self._weight_matrix = weights
+    def add_like(self,id:int) -> None:
+        if type(id) != int:
+            raise TypeError("id type must be int- type: ",type(id))
+        if id < 0:
+            raise ValueError("id must be above 0- id:" ,id)
+        self._recipes_liked.append(id)
+    def add_view(self,id:int) -> None:
+        if type(id) != int:
+            raise TypeError("id type must be int- type: ",type(id))
+        if id < 0:
+            raise ValueError("id must be above 0- id:" ,id)
+        self._recipes_viewed.append(id)
+    def add_make(self,id:int) -> None:
+        if type(id) != int:
+            raise TypeError("id type must be int- type: ",type(id))
+        if id < 0:
+            raise ValueError("id must be above 0- id:" ,id)
+        self._recipes_made.append(id)
+    def update_ingredients(self,food:dict) -> None:
+        for x,count in food.items():
+            self._ingredients_owned[x] += count
+    def update_weight_matrix(self,weights:list) -> None:
+        if type(weights) != list:
+            raise TypeError("type of weights must be list",type(weights))
+        if len(weights) != 4:
+            raise ValueError("list must be weight 4- len: ",len(weights))
+        self._weight_matrix = weights
     
-    # #given a list of top ingredients, return a dict that measures favorability of certain ingredients (0-1)
-    # #uses inverse tan function to map 0-infinity to 0-1
-    # #eg [corn,garlic] = [0.1,0.9]
-    # def get_favorability_array(self,relevant_ingredients: list,recipes:DataFrame) -> np.array:
-    #     #relevant_ingredients: a list of ingredient (strings) to care about
-    #     #recipes: a pandas series of lists of ingredient (strings)
-    #     favor = dict.fromkeys(relevant_ingredients,0)
-    #     tanfunc = lambda x: np.arctan(x/5)/ (np.pi/2)
-    #     for recipe in self._recipes_liked:
-    #         query = recipes.query('id ==' + str(recipe))['ingredients']
-    #         if len(query) != 1:
-    #             raise ValueError("id isn't unique")
-    #         for ing in query.iloc()[0]:
-    #             if ing in favor:
-    #                 favor[ing] += self._weight_matrix[0]
-    #     for recipe in self._recipes_made:
-    #         query = recipes.query('id ==' + str(recipe))['ingredients']
-    #         if len(query) != 1:
-    #             raise ValueError("id isn't unique")
-    #         for ing in query.iloc()[0]:
-    #             if ing in favor:
-    #                 favor[ing] += self._weight_matrix[1]
-    #     for recipe in self._recipes_viewed:
-    #         query = recipes.query('id ==' + str(recipe))['ingredients']
-    #         if len(query) != 1:
-    #             raise ValueError("id isn't unique")
-    #         for ing in query.iloc()[0]:
-    #             if ing in favor:
-    #                 favor[ing] += self._weight_matrix[2]
-    #     for ing in self._ingredients_owned.keys():
-    #         if ing in favor:
-    #             favor[ing] += self._weight_matrix[3] * self._weight_matrix[3]
-    #     return tanfunc(np.fromiter(favor.values(), dtype=float)).astype(float)
+    #given a list of top ingredients, return a dict that measures favorability of certain ingredients (0-1)
+    #uses inverse tan function to map 0-infinity to 0-1
+    #eg [corn,garlic] = [0.1,0.9]
+    def get_favorability_array(self,relevant_ingredients: list,recipes:DataFrame) -> np.array:
+        #relevant_ingredients: a list of ingredient (strings) to care about
+        #recipes: a pandas series of lists of ingredient (strings)
+        favor = dict.fromkeys(relevant_ingredients,0)
+        tanfunc = lambda x: np.arctan(x/5)/ (np.pi/2)
+        for recipe in self._recipes_liked:
+            query = recipes.query('id ==' + str(recipe))['ingredients']
+            if len(query) != 1:
+                raise ValueError("id isn't unique")
+            for ing in query.iloc()[0]:
+                if ing in favor:
+                    favor[ing] += self._weight_matrix[0]
+        for recipe in self._recipes_made:
+            query = recipes.query('id ==' + str(recipe))['ingredients']
+            if len(query) != 1:
+                raise ValueError("id isn't unique")
+            for ing in query.iloc()[0]:
+                if ing in favor:
+                    favor[ing] += self._weight_matrix[1]
+        for recipe in self._recipes_viewed:
+            query = recipes.query('id ==' + str(recipe))['ingredients']
+            if len(query) != 1:
+                raise ValueError("id isn't unique")
+            for ing in query.iloc()[0]:
+                if ing in favor:
+                    favor[ing] += self._weight_matrix[2]
+        for ing in self._ingredients_owned.keys():
+            if ing in favor:
+                favor[ing] += self._weight_matrix[3] * self._weight_matrix[3]
+        return tanfunc(np.fromiter(favor.values(), dtype=float)).astype(float)
 
 
